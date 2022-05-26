ef6a464aa36a6a09f28dcd4598f74c466aef4680
Bren30Lemar <104523006+Bren30Lemar@users.noreply.github.com>
2022-05-26 13:54:56 -0500

    Add files via upload

--- Dashboard/app.R
+++ Dashboard/app.R
@@ -6,67 +6,70 @@ library(DT)
 
 
 Cambio <- read_csv("Divisas_pandemia.csv")
-por <- read_table("Portada.txt")
 
-
-
-ui<-dashboardPage(title = "Ejemplo", skin = "purple",
-    dashboardHeader(title = "PROYECTO",
-                    dropdownMenu(type = "messages",
-                                 messageItem(from = "Brenda",
-                                             "Hola")
-                                 ),
-                    dropdownMenu(type = "notifications", 
-                                 notificationItem(text = "Trabajo")),
-                    dropdownMenu(type = "tasks",
-                                 taskItem(value = 50,
-                                          text = "avance",
-                                 color = "blue"))
-                    ),
-    dashboardSidebar(
-        sidebarSearchForm("searchText","buttonSearch","Buscar"),
-        sidebarMenu(id="sidebarID",
-                    menuItem("Portada", tabName = "por"),#Texto
-                    menuItem("Usuarios", tabName = "datos",icon = icon("users")),
-                    menuItem("Nodos"),
-                    menuItem("Reglamento"),
-                    menuItem("Problemas"),
-                    menuItem("Software", tabName = "datos1"), 
-                    menuItem("Paquetes R", tabName = "datos2"),
-                    menuItem("Contacto",
-                             menuSubItem("contacto", tabName = "imag" )), #  texto
-                    menuItem("Agradecimientos") # Texto
-                     )
-                    ),
-
-    dashboardBody(
-        tabItems(tabItem(tabName = "datos",
-                         DT::dataTableOutput("datos"))
-                 ),
-        tabItems(tabItem(tabName = "datos1",
-                         DT::dataTableOutput("datos1"))),
-        tabItems(tabItem(tabName = "datos2",
-                         DT::dataTableOutput("datos2"))),
-        tabItem(tabName = "imag",
-                box(title = "Contacto", tags$image(type = "image/png", src = "Diseño sin título.png",
-                                             controls = "controls", height = "400px", width = "600px"),
-                    width = 12,status = "primary", solidHeader = T)),
-       tabItem(tabName = "por",
-               verbatimTextOutput("por") )
-    )
-)
-
+ui<-dashboardPage(title= "Ejemplo", skin= "purple",
+                  dashboardHeader(title="PROYECTO",
+                                  dropdownMenu(type="messages",
+                                               messageItem(from = "Brenda",
+                                                           "Hola")
+                                  ),
+                                  dropdownMenu(type="notifications",
+                                               notificationItem(text="No te olvides de seguir")
+                                               
+                                  ),
+                                  dropdownMenu(type="tasks",
+                                               taskItem(value=50,
+                                                        text="Avance",
+                                                        color="red")
+                                  )
+                                  
+                  ),
+                  dashboardSidebar(
+                      sidebarSearchForm("searchText","buttonSearch","Buscar")
+                      ,
+                      sidebarMenu(id="sidebarID",
+                                  menuItem("Portada"), # Texto
+                                  menuItem("Usuarios", tabName = "datos", icon = icon("Users")),# Tabla
+                                  menuItem("Nodos", tabName = "datos1"), # Tabla
+                                  menuItem("Reglamento"), # Texto
+                                  menuItem("Problemas", tabName = "datos2"), # Tabla
+                                  menuItem("Contacto", tabName = "imag"), # Imagen
+                                  menuItem("Agradecimientos") # Texto
+                                  
+                                  
+                      )
+                      
+                      
+                  ),
+                  dashboardBody(
+                      
+                      tabItems(tabItem(tabName = "datos", 
+                                       DT::dataTableOutput("datos")
+                                       
+                                       
+                      ),
+                      tabItem(tabItem(tabName = "datos1",
+                                      DT::dataTableOutput("datos1"))
+                      ),
+                      
+                      tabItem(tabItem(tabName = "datos2",
+                                      DT::dataTableOutput("datos2"))
+                      ), 
+                      tabItem(tabName="imag", 
+                              box(title="Contaco", tags$image(type = "image/png",src = "Diseño sin título.png",
+                                                              controls = "controls", height="400px", width="600px"), 
+                                  width=12, status="primary", solidHeader=T))
+                      
+                      )
+                  ))
 server <- function(input, output) { 
-    output$datos<- DT::renderDataTable(Cambio)
+    output$datos <- DT::renderDataTable(Cambio)
     output$datos1 <- DT::renderDataTable(Cambio)
     output$datos2 <- DT::renderDataTable(Cambio)
     output$imag <- renderImage({
         return(list(src = "www/Diseño sin título.png", contentType = "image/png"))
-            })
-    output$por <- renderPrint({
-        input$por
     })
-
+    
 }
 
 
